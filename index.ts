f7fa7241b24d8740b7b1546b729ec768f1257531
umairx97 <umairahmedbajwa97@gmail.com>
2021-04-17 06:34:30 +0500

    feat: add code to run blockchain

--- index.ts
+++ index.ts
@@ -1,8 +1,20 @@
 import Blockchain from './lib/blockchain'
-import Block from './lib/block'
+import Transaction from './lib/transaction'
+import { KeyPair } from './lib/elliptic'
 
-const BNB = new Blockchain()
-BNB.addBlock(new Block(1, Date.now(), { amount: 10 }))
-BNB.addBlock(new Block(2, Date.now(), { amount: 10 }))
+const nodeCoin = new Blockchain()
 
-console.log(BNB.chain)
+const myKey = KeyPair.keyFromPrivate('e8da4ce84c5711abf5b3e086c0cf0686375d3985dafbd8d1573501373d90c1f0')
+const myWalletAddress = myKey.getPublic('hex')
+const toAddress = '0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7'
+const rewardAddress = toAddress
+const amount = 20
+
+const trx = new Transaction(myWalletAddress, toAddress, amount)
+trx.signTrx(myKey)
+nodeCoin.addTransaction(trx)
+
+nodeCoin.minePendingTransactions(rewardAddress)
+
+console.log({ chain: nodeCoin.chain })
+
